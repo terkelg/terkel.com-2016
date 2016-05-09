2af9c93205eb2252ef9a0a8910537fbbc46256bb
Terkel Gjervig <terkel@terkel.com>
2016-05-09 20:53:58 +0200

    Initial commit with routing

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// Components
+import App from './App'
+
+var Home = Vue.extend({
+  template: '<p>This is Home</p>'
+})
+
+var Bar = Vue.extend({
+  template: '<p>This is bar!</p>'
+})
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+  hashbang: false,
+  history: true,
+  pushstate: true
+})
+
+router.map({
+  '/': {
+    name: 'home',
+    component: Home
+  },
+  '/bar': {
+    name: 'bar',
+    component: Bar
+  }
+})
+
+// afterEach, beforeEach
+// Check out Vue.transitions
+/*
+router.beforeEach(function (route) {
+  console.log('Router:', route)
+})
+*/
+
+router.start(App, '#app')
+
+/* eslint-disable no-new */
+/*
+new Vue({
+  el: 'body',
+  components: { App }
+});
+*/
