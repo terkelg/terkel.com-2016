eac1c9bb776d111fc45373c6aad57ffeff256f06
Terkel Gjervig <terkel@terkel.com>
2016-05-12 23:52:38 +0200

    A better desktop navbar now.

--- src/components/Secondary.vue
+++ src/components/Secondary.vue
@@ -1,23 +1,31 @@
 <template>
+  <!-- Desktop Secondary -->
   <aside class="secondary" v-el:secondary>
     <div class="secondary__inner" v-el:inner>
-      <nav class="navigation" v-el:nav v-on:click.stop='clickNavigation'>
 
-        <div class="cases-nav">
+      <div class="navigation" v-el:nav v-on:click.stop='clickNavigation'>
+        <div class="content">
+
+          <nav class="top-buttons">
+            buttons
+          </nav>
+
+          <nav class="links">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Cases</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+
+          <div class="bottom-content">
+            2016
+          </div>
+
         </div>
+      </div>
 
-        <ul>
-          <li>Home</li>
-          <li>Cases</li>
-          <li>About</li>
-          <li>Contact</li>
-        <li>
-
-        <img src="">
-
-        <a v-on:click.stop='clickNavigation'>Sidebar</a>
-
-      </nav>
     </div>
   </aside>
 </template>
@@ -50,8 +58,8 @@ export default {
 
   methods: {
     clickNavigation () {
-      console.log('You clicked the nav: ', this.sizeTween);
-
+      // TODO: There's a big when you resize to a breakpoint and open/close menu
+      // the solution could be two different components. One loaded/visble if mobile, other if desktop.
       if (!this.secondaryIsOpen) {
         this.sizeTween.restart();
         this.colorTween.restart(); // reverse()
@@ -84,32 +92,86 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import '../variables';
 
-  // Naviger med GreenSock så jeg får callback
   .secondary {
     position: fixed;
     padding-right: $border-size;
     width: $nav-width + $border-size * 2;
+    height: 100%;
     &.open { width: 100%; }
+  }
 
-    .secondary__inner {
-      background-color: $white;
+  .secondary__inner {
+    background-color: $white;
+    width: 100%;
+    width: calc(100% - 1px); // Hide left border
+    height: 100%;
+    padding-top: $border-size;
+    padding-bottom: $border-size;
+    padding-left: 0;
+    padding-right: 0;
+    &.open { background-color: $dark; }
+  }
+
+  .navigation {
+    position: relative;;
+    width: $nav-width;
+    height: 100%;
+    float: right;
+    text-align: center;
+    border-right: 1px solid pink;
+    border-left: 1px solid green;
+
+    .content {
+      background-color: green;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+
+    .top-buttons {
+      position: absolute;
+      top: 10px;
+      left: 0;
       width: 100%;
-      height: 100vh;
-      padding-top: $border-size;
-      padding-bottom: $border-size;
-      &.open { background-color: $dark; }
+      background-color: red;
+    }
 
-      .navigation {
-        width: $nav-width;
-        height: 100%;
-        float: right;
-        padding: 8px;
-        border-right: 1px solid pink;
-        border-left: 1px solid green;
+    .links {
+      position: absolute;
+      top: 51%;
+      margin: -45px 0 0 -366px;
+      transform: rotate(90deg);
+      width: 800px;
+      //margin-left: -315px;
+
+      ul {
+        text-align: center;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+      }
+      li {
+        display: inline-block;
+        a {
+          padding: 0 20px;
+          color: red;
+          text-decoration: none;
+        }
       }
     }
+
+    .bottom-content {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      background-color: red;
+    }
+
   }
 </style>
