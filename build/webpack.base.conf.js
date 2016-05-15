fa9d7ecbc5dc1e7794935291c477d382ebec0d06
Terkel Gjervig <terkel@terkel.com>
2016-05-15 16:51:28 +0200

    Forgot something

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,12 +13,15 @@ module.exports = {
     filename: '[name].js'
   },
   resolve: {
+    root: path.resolve( __dirname, '..', 'src' ),
     extensions: ['', '.js', '.vue'],
     fallback: [path.join(__dirname, '../node_modules')],
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'core': path.resolve(__dirname, '../src/core'),
+      'modules': path.resolve(__dirname, '../src/modules')
     }
   },
   resolveLoader: {
