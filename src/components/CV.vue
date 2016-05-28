74425a4b101249d31274adf13d70c139956da635
Terkel Gjervig <terkel@terkel.com>
2016-05-28 11:35:05 +0200

    CV page also fades in/out now. Also wait on images

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -3,6 +3,11 @@
     This is the about page!
     <img src="https://placekitten.com/800/250" class="cover">
     <img src="https://placekitten.com/550/250" class="cover">
+    <img src="https://placekitten.com/550/250" class="cover">
+    <img src="https://placekitten.com/553/270" class="cover">
+    <img src="https://placekitten.com/550/260" class="cover">
+    <img src="https://placekitten.com/552/280" class="cover">
+    <img src="https://placekitten.com/551/211" class="cover">
   </div>
 </template>
 
@@ -11,4 +16,38 @@
 </style>
 
 <script>
+import imagesLoaded from 'imagesloaded';
+
+export default {
+  events: {
+    'route-change': function (route) {
+      return true;
+    },
+    'secondary-opened': function () {
+      console.log('CASE: Secondary Opnened - Im case!!');
+      return true;
+    }
+  },
+
+  ready () {
+    console.log('Ready CASE');
+  },
+
+  route: {
+    activate: function ({ next }) {
+      imagesLoaded(this.$el, () => {
+        next();
+        console.log('About with images loaded');
+      });
+    },
+    canDeactivate: function (transition) {
+      TweenLite.to(this.$el, 0.3, {
+        opacity: 0,
+        onComplete: () => {
+          transition.next();
+        }
+      });
+    }
+  }
+};
 </script>
