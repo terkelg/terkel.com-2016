99998f1fb5d2813d981ea32659e1bb9b459a2681
Terkel Gjervig <terkel@terkel.com>
2016-05-30 02:27:08 +0200

    Styling

--- src/components/stages/about.vue
+++ src/components/stages/about.vue
@@ -7,9 +7,9 @@
           <img src="http://placehold.it/150x150">
         </div>
         <div class="content">
-          <h2>Dabbler in all things creative</h2>
+          <h2 class="boxed">Dabbler in all things creative</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae nunc lacinia, commodo metus nec, auctor ex. Quisque vulputate arcu lorem, vel convallis ipsum blandit et. Quisque a magna ac nibh commodo congue eu vitae dolor.</p>
-          <a class="line" v-link="{name: 'cv'}">PSST There's more. Click me, I dare you</a>
+          <a class="line" v-link="{name: 'cv'}">There's more. Click me, I dare you</a>
         </div>
       </div>
 
