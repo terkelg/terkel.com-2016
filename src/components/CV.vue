9fa63c1ee44f1ed8820af24cbabd75a7b8563758
Terkel Gjervig <terkel@terkel.com>
2016-05-30 21:00:53 +0200

    Fixed media-query, added styling for CV page

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -1,13 +1,57 @@
 <template>
   <div class="secondary-content secondary-content--cv" v-show="open" transition="fade" transition-mode="out-in">
-    This is the about page!
-    <img src="https://placekitten.com/800/250" class="cover">
-    <img src="https://placekitten.com/550/250" class="cover">
-    <img src="https://placekitten.com/550/250" class="cover">
-    <img src="https://placekitten.com/553/270" class="cover">
-    <img src="https://placekitten.com/550/260" class="cover">
-    <img src="https://placekitten.com/552/280" class="cover">
-    <img src="https://placekitten.com/551/211" class="cover">
+
+    <article class="article">
+      <header class="head">
+        <img src="http://cdn.wonderfulengineering.com/wp-content/uploads/2014/06/Windows-XP-wallpaper.jpg" class="cover">
+        <div class="head__content">
+          <h1>Child of the internet</h1>
+          <span class="line">Turned out better than you expected</span>
+        </div>
+      </header>
+
+      <section class="body">
+        <p>
+        Hello, this is the radio24syv case
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Cras sed tincidunt enim. Aenean ante quam, euismod in felis at, porta mol
+        estie augue. Nunc ligula quam, varius in mi quis, finibus malesuada sem. Nunc sce
+        lerisque turpis turpis, sed vehicula sem pharetra vel. Morbi
+      </p>
+
+      <footer class="body__details">
+        <ul class="body__details__columns">
+          <li>
+            <h5 class="type">Experience</h5>
+            <ul class="details-list">
+              <li>Molamil</li>
+            </ul>
+          </li>
+          <li>
+            <h5 class="type">Clients</h5>
+            <ul class="details-list">
+              <li></li>
+              <li>After Effects</li>
+              <li>JavaScript</li>
+              <li>Swfit</li>
+              <li>3D</li>
+            </ul>
+          </li>
+          <li>
+            <h5 class="type">Tools</h5>
+            <ul class="details-list">
+              <li>Cinema 3D</li>
+              <li>After Effects</li>
+              <li>JavaScript</li>
+              <li>Swfit</li>
+              <li>3D</li>
+            </ul>
+          </li>
+        </ul>
+      </footer>
+
+    </article>
+
   </div>
 </template>
 
